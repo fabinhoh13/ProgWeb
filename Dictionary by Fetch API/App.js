ee0e53b02ef1b6e144c2539a1fe3025e00948cea
Fabio Henrique Alves Fernandes <58605084+fabinhoh13@users.noreply.github.com>
2023-02-15 14:57:35 -0300

    Add files via upload

---
+++ Dictionary by Fetch API/App.js
@@ -0,0 +1,53 @@
+const API_URL = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+
+class App {
+    constructor () {
+        this._onJsonReady = this._onJsonReady.bind(this);
+        this._onSubmit = this._onSubmit.bind (this);
+        
+        this.words = [];
+        this.descriptions = [];
+
+        const form = document.querySelector ('form');
+        form.addEventListener('submit', this._onSubmit);
+    }
+
+    _onSubmit (event) {
+        event.preventDefault();
+        const inputTextView = document.querySelector('#word');
+        const query = encodeURIComponent(inputTextView.value);
+
+        this.words = [];
+        this.descriptions = [];
+
+        fetch (API_URL + query)
+              .then (this._onResponse)
+              .then (this._onJsonReady);
+        
+    }
+
+    _renderWords () {
+        const wordsContainer = document.querySelector ('#Meanings');
+        wordsContainer.innerHTML = '';
+        for (var i = 0; i < this.words.length; i++){
+            const newWord = new Word (wordsContainer, this.words[i], this.descriptions[i]);
+        }
+    }
+
+    _onJsonReady (json) {
+        if (json === undefined){
+            return;
+        }
+        console.log (json);
+        const word = json[0].word;
+        const meaning = json[0].meanings[0].definitions[0].definition;
+        console.log(word + '\n' + meaning);
+        this.words.push(word);
+        this.descriptions.push(meaning);
+        this._renderWords();
+    }
+
+    _onResponse (response) {
+        return response.json();
+    }
+}
